23db711ba2114e8ab82853b0c3d304fa2e482e15
eri <eri0.sagittarius@gmail.com>
2021-05-17 22:55:06 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+
+namespace dotnet_reading_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use using statement with class that implements Dispose.
+            //StreamReader is a class which reads text files and found in the System.IO namespace
+            using (StreamReader reader = new StreamReader("./fruits.txt"))
+        {
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                Console.WriteLine(line); 
+            }
+        }
+        }
+    }
+}
